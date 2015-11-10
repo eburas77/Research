cad58c4cbd5c05ef21a980685d34edb9774f92b7
Eric Buras <ericburas@Erics-MacBook-Pro-4.local>
2015-11-10 16:36:57 -0600

    first

---
+++ powerlawgraph.py
@@ -0,0 +1,38 @@
+#create power law graph
+
+from networkx import *
+import sys
+
+n=20 # Number of nodes
+m=5 # Number of edges to attach from a new node to existing nodes
+
+G = barabasi_albert_graph(n,m,.5)
+
+# some properties
+print("node degree clustering")
+for v in nodes(G):
+    print('%s %d %f' % (v,degree(G,v),clustering(G,v)))
+
+# print the adjacency list to terminal
+try:
+    write_adjlist(G, sys.stdout)
+except TypeError: # Python 3.x
+    write_adjlist(G,sys.stdout.buffer)
+
+A = adjacency_matrix(G)
+
+#print("adjacency matrix of whole graph")
+#print(A)
+
+print("")
+
+k = 3
+l=3
+H = kl_connected_subgraph(G, k, l, low_memory=False, same_as_graph=False)
+
+A_H = adjacency_matrix(H)
+
+try:
+    write_adjlist(H, sys.stdout)
+except TypeError: # Python 3.x
+    write_adjlist(H,sys.stdout.buffer)
