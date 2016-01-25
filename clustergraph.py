67e8755d660c6890a9c7dbdad8fb5003a847fba7
Eric Buras <eburas77@gmail.com>
2016-01-25 14:35:01 -0500

    added comments

--- clustergraph.py
+++ clustergraph.py
@@ -4,15 +4,16 @@ import numpy as np
 from scipy import *
 import matplotlib.pyplot as plt
 
+
+#using Barabasi Albert Model or to create simple graph
 #G=Graph()
-
 #G.add_nodes_from([1,2,3,4,5,6,7,8,9])
-
 #G.add_edges_from([(1,2),(1,3),(1,5),(1,9),(2,3),(2,6),(2,8),(3,4),(4,5),(4,7),(4,9),(5,8),(6,7),(6,9)])
 #n=200# Number of nodes
 #m=5 # Number of edges to attach from a new node to existing nodes
-
 #G = barabasi_albert_graph(n,m,.5)
+
+#to create power law degree graph
 nodes = 1000
 beta = 2.5 #power law exponent
 z=[]
@@ -23,6 +24,9 @@ while len(z)<nodes:
 G = nx.configuration_model(z)
 G=nx.Graph(G) # remove parallel edges
 G.remove_edges_from(G.selfloop_edges())
+#this sometimes results in an error because the sum of node degrees is odd
+#run again until it is even
+
 
 #for v in nodes(G):
 #    print('%s %d %f' % (v,degree(G,v),clustering(G,v)))
@@ -33,6 +37,7 @@ print "this is the laplacian matrix of G"
 L= L.todense()
 print L
 
+# L = L + I to avoid singularity
 L = L+eye(len(L))
 
 #eigs_L = laplacian_spectrum(G)
@@ -44,44 +49,44 @@ k = 3 #A higher number means a looser connectivity requirement.
 l = 3 #A higher number means a stricter connectivity requirement.
 
 #this is fan chungs greedy algorithm
-H = kl_connected_subgraph(G, k, l, low_memory=False, same_as_graph=False)
+P = kl_connected_subgraph(G, k, l, low_memory=False, same_as_graph=False)
 
-H_L = laplacian_matrix(H)
-H_L = H_L.todense()
+P_L = laplacian_matrix(P)
+P_L = P_L.todense()
 
-
-T = L-H_L
-H_L = H_L+eye(len(L))*diagonal(T)
+#small changes to keep the degree sequence in P
+T = L-P_L
+P_L = P_L+eye(len(L))*diagonal(T)
 T = T-eye(len(L))*diagonal(T)
 print "this is the laplacian matrix of the maximum k,l connected subgraph" 
-print H_L
+print P_L
 print ""
 print "this is the teleportation matrix"
 print T
 print ""
 
+#we would do multigrid here
+P_L_inv = np.linalg.inv(P_L)
+P_L_inv = np.array(P_L_inv)
 
-H_L_inv = np.linalg.inv(H_L)
-H_L_inv = np.array(H_L_inv)
-
-
+#SVD
 U,s,V = np.linalg.svd(T)
 size = sum(s>.00000001)
 
+#remove rows and columns for low rank matrix
 U = np.array(U[:,0:size-1])
 s = s[0:size-1]
 s = np.diag(s)
 #s = np.reshape(len(s),1)
 V = np.array(V[0:size-1,:])
 
-a = np.dot(H_L_inv,U)
-b = np.dot(V,H_L_inv)
-c = np.dot(V,H_L_inv)
+#SHERMAN WOODBERRY MORRISON
+a = np.dot(P_L_inv,U)
+b = np.dot(V,P_L_inv)
+c = np.dot(V,P_L_inv)
 d = np.dot(c,U)
+solve_L = P_L_inv + np.dot(np.dot(a,(np.linalg.inv(np.linalg.inv(s)-d))),b)
 
-
-
-solve_L = H_L_inv + np.dot(np.dot(a,(np.linalg.inv(np.linalg.inv(s)-d))),b)
 
 L_inv = np.linalg.inv(L)
 
