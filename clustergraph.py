1a331b6f455bdd707f99dcdeb2577f04dd6abdb3
Eric Buras <eburas77@gmail.com>
2016-02-02 17:10:36 -0600

    changes

--- clustergraph.py
+++ clustergraph.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 
 #using Barabasi Albert Model or to create simple graph
 G=Graph()
-G.add_nodes_from([1:24])
+G.add_nodes_from(range(1,25))
 G.add_edges_from([(1,2),(2,3),(3,4),(1,5),(2,6),(3,7),(4,8),(5,6),(6,7),(7,8),
                     (5,9),(6,10),(7,11),(8,12),(9,10),(10,11),(11,12),
-                    (9,13),(10,14),(11,15),(12,16),(13,14),(14,15),(15,16)
+                    (9,13),(10,14),(11,15),(12,16),(13,14),(14,15),(15,16),
                     (1,21),(4,20),(4,24),(14,23),(15,22),(16,17),(3,19),(11,18)])
 #n=200# Number of nodes
 #m=5 # Number of edges to attach from a new node to existing nodes
