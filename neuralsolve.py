297d2ceb66e9cd3c8f9c1b6dd14f745614ba8eb8
Eric Buras <eburas77@gmail.com>
2016-03-29 14:39:04 -0500

    second draft

--- neuralsolve.py
+++ neuralsolve.py
@@ -152,10 +152,10 @@ timesvd = timeit.default_timer()-time2
 print "timesvd: ", timesvd
 size = sum(s>.00000001)
 
-plt.semilogy(s)
-plt.ylabel("Singular Value")
-plt.xlabel("Neural Network Singular Values")
-plt.savefig('neuralsing.png')
+#plt.semilogy(s)
+#plt.ylabel("Singular Value")
+#plt.xlabel("Neural Network Singular Values")
+#plt.savefig('neuralsing.png')
 
 
 #remove rows and columns for low rank matrix
